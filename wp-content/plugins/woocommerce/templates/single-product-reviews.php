e90ba02726ed946e0931a724ee6d63dca61eeb94
IBrianMendoza <ibrianmendoza@gmail.com>
2019-07-18 11:56:16 -0600

    Reseñas de los proyectos

--- wp-content/plugins/woocommerce/templates/single-product-reviews.php
+++ wp-content/plugins/woocommerce/templates/single-product-reviews.php
@@ -33,10 +33,10 @@ if ( ! comments_open() ) {
 			$count = $product->get_review_count();
 			if ( $count && wc_review_ratings_enabled() ) {
 				/* translators: 1: reviews count 2: product name */
-				$reviews_title = sprintf( esc_html( _n( '%1$s review for %2$s', '%1$s reviews for %2$s', $count, 'woocommerce' ) ), esc_html( $count ), '<span>' . get_the_title() . '</span>' );
+				$reviews_title = sprintf( esc_html( _n( '%1$s reseña por %2$s', '%1$s reseñas por %2$s', $count, 'woocommerce' ) ), esc_html( $count ), '<span>' . get_the_title() . '</span>' );
 				echo apply_filters( 'woocommerce_reviews_title', $reviews_title, $count, $product ); // WPCS: XSS ok.
 			} else {
-				esc_html_e( 'Reviews', 'woocommerce' );
+				esc_html_e( 'Reseñas', 'woocommerce' );
 			}
 			?>
 		</h2>
@@ -63,7 +63,7 @@ if ( ! comments_open() ) {
 			endif;
 			?>
 		<?php else : ?>
-			<p class="woocommerce-noreviews"><?php esc_html_e( 'There are no reviews yet.', 'woocommerce' ); ?></p>
+			<p class="woocommerce-noreviews"><?php esc_html_e( 'No hay reseñas aún.', 'woocommerce' ); ?></p>
 		<?php endif; ?>
 	</div>
 
@@ -75,19 +75,19 @@ if ( ! comments_open() ) {
 
 				$comment_form = array(
 					/* translators: %s is product title */
-					'title_reply'         => have_comments() ? __( 'Add a review', 'woocommerce' ) : sprintf( __( 'Be the first to review &ldquo;%s&rdquo;', 'woocommerce' ), get_the_title() ),
+					'title_reply'         => have_comments() ? __( 'Añadir reseña', 'woocommerce' ) : sprintf( __( 'Be the first to review &ldquo;%s&rdquo;', 'woocommerce' ), get_the_title() ),
 					/* translators: %s is product title */
-					'title_reply_to'      => __( 'Leave a Reply to %s', 'woocommerce' ),
+					'title_reply_to'      => __( 'Dejar una reseña  %s', 'woocommerce' ),
 					'title_reply_before'  => '<span id="reply-title" class="comment-reply-title">',
 					'title_reply_after'   => '</span>',
 					'comment_notes_after' => '',
 					'fields'              => array(
-						'author' => '<p class="comment-form-author"><label for="author">' . esc_html__( 'Name', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label> ' .
+						'author' => '<p class="comment-form-author"><label for="author">' . esc_html__( 'Nombre', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label> ' .
 									'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" required /></p>',
 						'email'  => '<p class="comment-form-email"><label for="email">' . esc_html__( 'Email', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label> ' .
 									'<input id="email" name="email" type="email" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30" required /></p>',
 					),
-					'label_submit'        => __( 'Submit', 'woocommerce' ),
+					'label_submit'        => __( 'Hecho', 'woocommerce' ),
 					'logged_in_as'        => '',
 					'comment_field'       => '',
 				);
@@ -95,28 +95,28 @@ if ( ! comments_open() ) {
 				$account_page_url = wc_get_page_permalink( 'myaccount' );
 				if ( $account_page_url ) {
 					/* translators: %s opening and closing link tags respectively */
-					$comment_form['must_log_in'] = '<p class="must-log-in">' . sprintf( esc_html__( 'You must be %1$slogged in%2$s to post a review.', 'woocommerce' ), '<a href="' . esc_url( $account_page_url ) . '">', '</a>' ) . '</p>';
+					$comment_form['must_log_in'] = '<p class="must-log-in">' . sprintf( esc_html__( 'Debes de estar %1$sloggeado %2$s para publicar una reseña.', 'woocommerce' ), '<a href="' . esc_url( $account_page_url ) . '">', '</a>' ) . '</p>';
 				}
 
 				if ( wc_review_ratings_enabled() ) {
-					$comment_form['comment_field'] = '<div class="comment-form-rating"><label for="rating">' . esc_html__( 'Your rating', 'woocommerce' ) . '</label><select name="rating" id="rating" required>
+					$comment_form['comment_field'] = '<div class="comment-form-rating"><label for="rating">' . esc_html__( 'Tu valoración', 'woocommerce' ) . '</label><select name="rating" id="rating" required>
 						<option value="">' . esc_html__( 'Rate&hellip;', 'woocommerce' ) . '</option>
-						<option value="5">' . esc_html__( 'Perfect', 'woocommerce' ) . '</option>
-						<option value="4">' . esc_html__( 'Good', 'woocommerce' ) . '</option>
-						<option value="3">' . esc_html__( 'Average', 'woocommerce' ) . '</option>
-						<option value="2">' . esc_html__( 'Not that bad', 'woocommerce' ) . '</option>
-						<option value="1">' . esc_html__( 'Very poor', 'woocommerce' ) . '</option>
+						<option value="5">' . esc_html__( 'Perfecto', 'woocommerce' ) . '</option>
+						<option value="4">' . esc_html__( 'Bueno', 'woocommerce' ) . '</option>
+						<option value="3">' . esc_html__( 'Promedio', 'woocommerce' ) . '</option>
+						<option value="2">' . esc_html__( 'No tan mal', 'woocommerce' ) . '</option>
+						<option value="1">' . esc_html__( 'Muy pobre', 'woocommerce' ) . '</option>
 					</select></div>';
 				}
 
-				$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . esc_html__( 'Your review', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required></textarea></p>';
+				$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . esc_html__( 'Tu reseña', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required></textarea></p>';
 
 				comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
 				?>
 			</div>
 		</div>
 	<?php else : ?>
-		<p class="woocommerce-verification-required"><?php esc_html_e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ); ?></p>
+		<p class="woocommerce-verification-required"><?php esc_html_e( 'Solo los clientes registrados que hayan invertido en este proyecto pueden dejar un comentario.', 'woocommerce' ); ?></p>
 	<?php endif; ?>
 
 	<div class="clear"></div>
